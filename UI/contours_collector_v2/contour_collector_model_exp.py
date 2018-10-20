8acc55ca8ecf20d879e67a6013858288bb9d9474
Trevol <iam.vladimir.tretyak@gmail.com>
2018-10-20 10:54:14 +0300

    separate parameters view

---
+++ UI/contours_collector_v2/contour_collector_model_exp.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+from UI.contours_collector.contours_collector_model import ContoursCollectorModel
+
+
+def main():
+    frame = "D:/DiskE/Computer_Vision_Task/frames_2/f_483_32200.00_32.20.jpg"
+    #frame = "D:/DiskE/Computer_Vision_Task/frames_6/f_2770_184666.67_184.67.jpg"
+    # frame = "D:/DiskE/Computer_Vision_Task/frames_6/f_0068_4533.33_4.53.jpg"
+    model = ContoursCollectorModel(frame)
+
+    t0 = time.time()
+    contours, edges = model.find_contours()
+    print('contours', time.time() - t0)
+    contours = [contour for contour in contours if len(contour.points) >= 5]
+
+    t0 = time.time()
+    ellipses = [box_to_ellipse(*cv2.fitEllipseDirect(contour.points)) for contour in contours]
+    print('fitEllipseDirect', time.time() - t0)
+
+    t0 = time.time()
+    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+    for center, axes, angle in ellipses:
+        cv2.ellipse(edges, intt(center), intt(axes), angle, 0, 360, (255, 0, 0), 2)
+    print('draw ellipses', time.time() - t0)
+
+    for contour in contours:
+        contour.draw(edges, (0, 255, 0), 1)
+
+    cv2.imshow('ee', cv2.cvtColor(edges, cv2.COLOR_RGB2BGR))
+    cv2.waitKey()
+
+    ## area, number of points
+    # cv2.ellipse(img, center, axes, angle, startAngle, endAngle, color[, thickness[, lineType[, shift]]]) → img
+    # cv2.ellipse(img, box, color[, thickness[, lineType]]) → img
+
+
+main()
