7cec07fbb4bfca5c9d44b312911367cbcfa747ac
Trevol <iam.vladimir.tretyak@gmail.com>
2018-10-27 14:58:42 +0300

    keyboard frame by frame in backgroud substractor

--- backgroundSubtractor/BackgroundSubtractorGMG.py
+++ backgroundSubtractor/BackgroundSubtractorGMG.py
@@ -7,7 +7,7 @@ fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
 while (1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
-    fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+    #fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
     cv.imshow('frame', fgmask)
     k = cv.waitKey(30) & 0xff
     if k == 27:
--- backgroundSubtractor/BackgroundSubtractorMOG.py
+++ backgroundSubtractor/BackgroundSubtractorMOG.py
@@ -1,13 +1,24 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture("d:\DiskE\Computer_Vision_Task\Video 2.mp4")
-fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
-while(1):
-    ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    cv.imshow('frame',fgmask)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
-cap.release()
-cv.destroyAllWindows()
+
+
+def main():
+    cap = cv.VideoCapture("d:\DiskE\Computer_Vision_Task\Video 2.mp4")
+    fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+    while (1):
+        ret, frame = cap.read()
+        fgmask = fgbg.apply(frame)
+        putFrameNum(fgmask, cap)
+
+        cv.imshow('frame', fgmask)
+        k = cv.waitKey() & 0xff
+        if k == 27:
+            break
+    cap.release()
+    cv.destroyAllWindows()
+
+def putFrameNum(frame, video):
+    framePos = int(video.get(cv.CAP_PROP_POS_FRAMES))
+    cv.putText(frame, str(framePos), (15, 25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (155, 155, 155), thickness=2)
+
+main()
