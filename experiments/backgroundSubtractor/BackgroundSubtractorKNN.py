13b26c4c5cd54c014b36c849e66b8359accbb231
trevol <iamvovan@mail.ru>
2019-09-05 10:32:36 +0300

    fix paths

--- experiments/backgroundSubtractor/BackgroundSubtractorKNN.py
+++ experiments/backgroundSubtractor/BackgroundSubtractorKNN.py
@@ -1,6 +1,6 @@
 import numpy as np
-import cv2 as cv
+import cv2
 from bg_substractor_utils import do_substraction
 import video_sources
 
-do_substraction(video_sources.video_2, cv.createBackgroundSubtractorKNN())
+do_substraction(video_sources.video_2, cv2.createBackgroundSubtractorKNN(detectShadows=False))
